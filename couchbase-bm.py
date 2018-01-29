3b089f17bbeb1ed9cc347c913b856a583f786867
neerajprem <neerajpremverma@gmail.com>
2018-01-29 23:55:26 +0530

    cocuhbase-benchmar-v1

---
+++ couchbase-bm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from couchbase.cluster import Cluster, PasswordAuthenticator
+from random import *
+
+cluster = Cluster('couchbase://172.31.4.221')
+cluster.authenticate(PasswordAuthenticator('admin', 'froggy'))
+bucket = cluster.open_bucket('mf-test')
+
+for inc in range(1,100):
+    CBKEY = 'name'+str(random())
+    CBVALUE = 'amazing'+str(inc)
+    print CBKEY+' : '+CBVALUE
+    bucket.upsert(CBKEY,CBVALUE)
+    res = bucket.get(CBKEY)
+    print res.value
+
+print "Bye"
