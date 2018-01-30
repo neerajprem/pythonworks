02e26ba7cd465f90f00b82c8309227767ff77557
neerajprem <neerajpremverma@gmail.com>
2018-01-30 13:39:07 +0530

    Release v3 with thread count as param

--- couchbase-bm.py
+++ couchbase-bm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import thread
+import sys
 from couchbase.cluster import Cluster, PasswordAuthenticator
 from random import *
 
@@ -15,36 +16,18 @@ def cbkiller(ThreadName, StartVal , EndVal):
         res = bucket.get(CBKEY)
         print ThreadName+' -- '+CBKEY+' : '+res.value
 
+NumThreads = int(sys.argv[1])
+Doc_Count = int(sys.argv[2])
+StartVal = 1
 
-for i in range(1, num_of_threads):
+for i in range(1, NumThreads):
     try:
-        thread.start_new_thread(cbkiller, ("Thread-"+i, ))
+        thread.start_new_thread( cbkiller, ("Thread-"+str(i), StartVal, StartVal+Doc_Count, ) )
     except:
         print "Error: unable to start thread"
+    StartVal = StartVal + Doc_Count + 1
 
 while 1:
        pass
 
 print "Bye"
-
-# try:
-#   thread.start_new_thread( cbkiller, ("Thread-1", 1, 1000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-2", 1000001, 2000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-3", 2000001, 3000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-4", 3000001, 4000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-5", 4000001, 5000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-6", 5000001, 6000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-7", 6000001, 7000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-8", 7000001, 8000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-9", 8000001, 9000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-10", 9000001, 10000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-11", 10000001, 11000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-12", 11000001, 12000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-18", 17000001, 18000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-13", 12000001, 13000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-14", 13000001, 14000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-15", 14000001, 15000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-16", 15000001, 16000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-17", 16000001, 17000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-19", 18000001, 19000000, ) )
-#   thread.start_new_thread( cbkiller, ("Thread-20", 19000001, 20000000, ) ) 
