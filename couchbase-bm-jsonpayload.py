f5b830fae0a9ecb60f2ea19eb3893b868628dbde
neerajprem <neerajpremverma@gmail.com>
2018-01-31 02:13:08 +0530

    Release : Couchbase Benchmarking with JSON payload, Key Expiry & Increment in verbose mode.

--- couchbase-bm-jsonpayload.py
+++ couchbase-bm-jsonpayload.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python
 
-import sys
-import time
+import time, thread, sys, couchbase
 from couchbase.cluster import Cluster, PasswordAuthenticator
-import couchbase
+from random import *
+
+##Global Declarations -
+CouchbaseServerAddress = 'couchbase://<ip-address>'
+CouchbaseUsername = '<username>'
+CouchbasePassword = '<password>'
+CouchbaseBucketName = '<bucketname>'
 
 def CbClusterConnect():
-    cluster = Cluster('couchbase://<server-ip>')
-    cluster.authenticate(PasswordAuthenticator('username', 'password'))
+    cluster = Cluster(CouchbaseServerAddress)
+    cluster.authenticate(PasswordAuthenticator(CouchbaseUsername, CouchbasePassword))
     return cluster
-
-def KeepCheck(KeyToCheck):
-    MyCluster = CbClusterConnect()
-    bucket = MyCluster.open_bucket('<BucketName>')
-    CheckCounter = 1
-    while True :
-        print "Check "+str(CheckCounter)+" : "+bucket.get(KeyToCheck).value
-        time.sleep(1)
-        CheckCounter += 1
             
-def cbkiller( JSONKey , JSONVal):
+def cbkiller( ThreadNumber , StartVal, EndVal):
     MyCluster = CbClusterConnect()
-    bucket = MyCluster.open_bucket('<BucketName>')
-    bucket.upsert(JSONKey, JSONVal, ttl=600, format=couchbase.FMT_JSON)
-    print "Output : "+bucket.get(JSONKey).value
-    #KeepCheck(JSONKey)
+    bucket = MyCluster.open_bucket(CouchbaseBucketName)
+    for VALUE in range(StartVal,EndVal):
+        JSONKey = 'KEY-'+str(random())
+        JSONVal = VALUE
+        bucket.upsert(JSONKey, JSONVal, ttl=600, format=couchbase.FMT_JSON)
+        print "T# %d - %s : %s " % ( ThreadNumber, JSONKey, str(bucket.get(JSONKey).value) )
+        for BCI in range(5,25, 5):
+            NewValue = bucket.counter(JSONKey, delta=BCI)
+            print "T# %s - new : %d" %( ThreadNumber, NewValue.value)
 
-cbkiller(sys.argv[1], sys.argv[2])
+NumThreads = int(sys.argv[1]) + 1
+Doc_Count = int(sys.argv[2])
+Sval = 1
+Eval = Doc_Count
+
+for COUNT in range(1, NumThreads):
+    try:
+        thread.start_new_thread( cbkiller, (COUNT, Sval, Eval, ) )
+        Sval = Eval + 1
+        Eval = Eval + Doc_Count
+    except:
+        print "Error: unable to start thread"
+
+while 1:
+    pass
