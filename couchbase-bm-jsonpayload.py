1ca227925d90897a7cb96a5b9efd0dc6e63b19c7
neerajprem <neerajpremverma@gmail.com>
2018-02-01 00:34:28 +0530

    trying with new cbserver 171

--- couchbase-bm-jsonpayload.py
+++ couchbase-bm-jsonpayload.py
@@ -5,10 +5,11 @@ from couchbase.cluster import Cluster, PasswordAuthenticator
 from random import *
 
 ##Global Declarations -
-CouchbaseServerAddress = 'couchbase://<ip-address>'
-CouchbaseUsername = '<username>'
-CouchbasePassword = '<password>'
-CouchbaseBucketName = '<bucketname>'
+CouchbaseServerAddress = 'couchbase://172.31.15.171'
+CouchbaseUsername = 'admin'
+CouchbasePassword = 'froggy'
+CouchbaseBucketName = 'mfl-test'
+KeyExpireTime = 600
 
 def CbClusterConnect():
     cluster = Cluster(CouchbaseServerAddress)
@@ -18,14 +19,14 @@ def CbClusterConnect():
 def cbkiller( ThreadNumber , StartVal, EndVal):
     MyCluster = CbClusterConnect()
     bucket = MyCluster.open_bucket(CouchbaseBucketName)
-    for VALUE in range(StartVal,EndVal):
-        JSONKey = 'KEY-'+str(random())
-        JSONVal = VALUE
-        bucket.upsert(JSONKey, JSONVal, ttl=600, format=couchbase.FMT_JSON)
-        print "T# %d - %s : %s " % ( ThreadNumber, JSONKey, str(bucket.get(JSONKey).value) )
-        for BCI in range(5,25, 5):
-            NewValue = bucket.counter(JSONKey, delta=BCI)
-            print "T# %s - new : %d" %( ThreadNumber, NewValue.value)
+    while True :
+        for VALUE in range(StartVal,EndVal):
+            JSONKey = 'KEY-'+str(random())
+            JSONVal = VALUE
+            bucket.upsert(JSONKey, { "MOONFROG-"+str(JSONVal) : { JSONKey+"amazingvalue" : str( JSONVal*0.9)+"ThisMakesMagic" }, "ADDRESS" : { "Locality":"Cambridge Layout", "Area":"Indiranagar", "Pincode": 560043 }, "Employees" : "100-200", "WEBSITE" : "www.moonfroglabs.com", "CONTACT" : "9986239645", "DEPARTMENTS" : { "DevOps" : { "KeyPerson 1 " : "Neeraj Prem Verma" , "KeyPerson 2" : "Gaurav Tayal"}, "Account" : { "KeyPerson 1 " : "Ashwini Khemka" , "KeyPerson 2" : "Chetan Kejriwal"}, "Human Resource" : { "KeyPerson 1 " : "Deepa Naidu", "KeyPerson 2" : "Surabhi Aswal", "KeyPerson 3" : "Varsha Sharma", "KeyPerson 4" : "Samhita", "KeyPerson 5" : "Adarsh", "KeyPerson 6" : "Navjot Poddar", "Site Reliability Engineering" : { "KeyPerson 1 " : "Mohammad Shafin", "KeyPerson 2" : "Denis DSouza", "KeyPerson 3" : "Koushik Paul" } } } } , ttl = KeyExpireTime, format=couchbase.FMT_JSON)
+            print "T# %d - %s : %s " % ( ThreadNumber, JSONKey, str(bucket.get(JSONKey).value) )
+        time.sleep(10)
+    print "cbkiller over" 
 
 NumThreads = int(sys.argv[1]) + 1
 Doc_Count = int(sys.argv[2])
